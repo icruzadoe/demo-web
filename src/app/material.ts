ccfda6cc98f27450722b7c7f9ad7dec9c74470d2
ivan cruzado <Ivn.cruzado@vasslatam.com>
2019-10-20 19:04:02 -0500

    se agrego el modulo de material para hacer responsive el diseno

--- src/app/material.ts
+++ src/app/material.ts
@@ -4,18 +4,28 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { NgModule } from '@angular/core';
 
 @NgModule({
     imports: [
-        MatButtonModule,MatToolbarModule,MatIconModule,MatMenuModule,MatSidenavModule,
-        MatRadioModule
-        
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatRadioModule,
+        MatGridListModule,
     ],
     exports: [
-        MatButtonModule,MatToolbarModule,MatIconModule,MatMenuModule,MatSidenavModule,
-        MatRadioModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatRadioModule,
+        MatGridListModule,
     ],
 })
 
