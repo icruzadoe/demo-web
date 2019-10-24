be0a80ea217620eec5807f4472173ba009b7d3ea
ivan cruzado <Ivn.cruzado@vasslatam.com>
2019-10-24 05:09:35 -0500

    se redirige al login cuango ingreses a la web y cuando coloquescualquier cosa en la url

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,13 +5,16 @@ import { PublishtextComponent } from './../publishtext/publishtext.component';
 import { PublishvideoComponent } from './../publishvideo/publishvideo.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule, Component } from '@angular/core';
+import { AppComponent } from '../inicio/app.component';
 
 const routes: Routes = [
   {path: 'publishvideo', component: PublishvideoComponent},
   {path: 'publishtext', component: PublishtextComponent},
   {path: 'publishbanner', component: PublishbannerComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'registeruser', component: RegisteruserComponent}
+  {path: 'registeruser', component: RegisteruserComponent},
+  {path: '', component: LoginComponent,  pathMatch: 'full'},
+  {path: '**', redirectTo:'/',  pathMatch: 'full'}
 ]
 
 @NgModule({
