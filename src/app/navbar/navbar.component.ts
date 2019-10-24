83c752b87a8df7aad386040718febaed8159997b
ivan cruzado <Ivn.cruzado@vasslatam.com>
2019-10-24 18:14:38 -0500

    se agrego la funcionalidad de logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  loginData : any;
+  showButton : boolean;
+  isLogout : boolean = false;
   constructor() { }
 
   ngOnInit() {
+    this.loginData = JSON.parse(localStorage.getItem('login'));
+
+    if (this.loginData.user = ! null && this.loginData.auth) {
+      this.showButton = true;
+    }
+  }
+
+  login(event){
+    this.isLogout = true;
+  }
+
+  logout(event){
+    this.isLogout = false;
+    localStorage.clear();
   }
 
 }
